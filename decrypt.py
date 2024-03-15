ed9edf3916b270899f433a7f0ccc3661be3ee46e
BrendanA85 <160781664+BrendanA85@users.noreply.github.com>
2024-03-15 18:43:46 +0000

    have encryption and decryption working with spaces

--- decrypt.py
+++ decrypt.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python
 
+from maps import uppercase_letters_to_numbers,uppercase_numbers_to_letters
+letters_to_numbers = {
+    'a': 1, 'b' : 2, 'c' : 3, 'd':4,'e':5,'f':6,'g':7,'h':8, 'i':9,'j':10,'k':11, 'l':12, 'm':13, 'n':14, 'o':15, 'p':16, 'q':17, 'r':18, 's':19, 't':20, 'u':21, 'v':22, 'w':23, 'x':24, 'y':25, 'z':26
+}
+numbers_to_letters = {}
+for letter, number in letters_to_numbers.items():
+    numbers_to_letters[number] = letter
 
 def decrypt(ciphertext, shift=0):
     """
@@ -7,7 +14,18 @@ def decrypt(ciphertext, shift=0):
     :param ciphertext: The message to be encrypted
     :param shift: The number of positions to shift each letter
     """
-    return ciphertext
+    decrypted_string = ""
+    for letter in ciphertext:
+        if letter not in letters_to_numbers:
+            decrypted_string += letter
+            continue
+        shifted_letter = letters_to_numbers[letter]
+        unshifted_index = shifted_letter - shift
+        if unshifted_index < 1:
+            unshifted_index += 26
+        unshifted_letter = numbers_to_letters[unshifted_index]
+        decrypted_string += unshifted_letter
+    return decrypted_string
 
 
 if __name__ == "__main__":
--- encrypt.py
+++ encrypt.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+from maps import letters_to_numbers, numbers_to_letters,uppercase_letters_to_numbers,uppercase_numbers_to_letters
 
 def encrypt(plaintext, shift=0):
     """
@@ -7,7 +8,25 @@ def encrypt(plaintext, shift=0):
     :param plaintext: The message to be encrypted
     :param shift: The number of positions to shift each letter
     """
-    return plaintext
+    #create an empty string to store encrypted text
+    encrypted_string = ""
+    # loop through plaintext look at eache letter in order
+    for letter in plaintext:
+        if letter not in letters_to_numbers:
+            encrypted_string += letter
+            continue 
+        #look up the numeric representation for the letter (use dictionary)
+        unshifted_letter = letters_to_numbers[letter]
+        #add the shift to the numeric representation
+        shifted_index = unshifted_letter + shift
+        if shifted_index > 26:
+            shifted_index -= 26
+        #look up the letter associated with the shifted index(use other dictonary)
+        shifted_letter = numbers_to_letters[shifted_index]
+        #add shifted letter to encrypted string
+        encrypted_string += shifted_letter
+    #return the encrypted string
+    return encrypted_string
 
 
 if __name__ == "__main__":
